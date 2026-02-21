b3e4020cbf608441c7ef3514882218c4d89f69c3
sandhusahib <sahibsandhu108@gmail.com>
2026-02-21 13:45:33 +0000

    Plotting added to tests

--- tests.py
+++ tests.py
@@ -1,9 +1,15 @@
 import complexmath
+import matplotlib.pyplot as plt
 
 x = complexmath.Complex(2,-7.2)
 y = complexmath.Complex(-9, 3.6)
 z = complexmath.Complex(15.4,-8)
 
 print(complexmath.Complex())
-print(x+y+z)
+print(x*y-z)
 print(sum([x,y,z],start=complexmath.Complex(0)))
+
+X = [0,x.re]
+Y = [0,x.im]
+plt.plot(X,Y)
+plt.show()
