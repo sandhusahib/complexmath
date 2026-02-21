9b4c1b9dd624c975610483035d89f08e28880171
sandhusahib <sahibsandhu108@gmail.com>
2026-02-21 13:27:48 +0000

    Rename attributes for less ambiguity

--- complexmath.py
+++ complexmath.py
@@ -1,35 +1,35 @@
 class Complex:
     "Complex number definition"
     def __init__(self, real_part=0, imag_part=0):
-        self.r = real_part
-        self.i = imag_part
+        self.re = real_part
+        self.im = imag_part
 
     def __str__(self):
-        if self.r == 0 and self.i == 0:
+        if self.re == 0 and self.im == 0:
             complex_str = str(0)
-        elif self.r == 0 and self.i != 0:
-            complex_str = str(self.i) + "i"
-        elif self.i == 0 and self.r != 0:
+        elif self.re == 0 and self.im != 0:
+            complex_str = str(self.im) + "i"
+        elif self.im == 0 and self.re != 0:
             complex_str = str(self.r)
-        elif self.i > 0:
-            complex_str = str(self.r) + " + " + str(self.i) + "i"
+        elif self.im > 0:
+            complex_str = str(self.re) + " + " + str(self.im) + "i"
         else:
-            neg_im_pt = (-1) * self.i
-            complex_str = str(self.r) + " - " + str(neg_im_pt) + "i"
+            neg_im_pt = (-1) * self.im
+            complex_str = str(self.re) + " - " + str(neg_im_pt) + "i"
         return complex_str
     
     def __add__(self, other):
-        real_pt = self.r + other.r
-        imag_pt = self.i + other.i
+        real_pt = self.re + other.re
+        imag_pt = self.im + other.im
         return Complex(real_pt, imag_pt)
     
     def __sub__(self, other):
-        real_pt = self.r - other.r
-        imag_pt = self.i - other.i
+        real_pt = self.re - other.re
+        imag_pt = self.im - other.im
         return Complex(real_pt, imag_pt)
     
     def __mul__(self, other):
-        real_pt = (self.r * other.r) - (self.i * other.i)
-        imag_pt = (self.r * other.i) + (self.i * other.r)
+        real_pt = (self.re * other.re) - (self.im * other.im)
+        imag_pt = (self.re * other.im) + (self.im * other.re)
         return Complex(real_pt, imag_pt)
     
