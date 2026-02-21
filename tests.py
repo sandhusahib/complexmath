a582a4c3a3ccbffb8540058a2a2c89895968742d
sandhusahib <sahibsandhu108@gmail.com>
2026-02-21 05:09:37 +0000

    Move tests to tests.py

---
+++ tests.py
@@ -0,0 +1,9 @@
+import complexmath
+
+x = complexmath.Complex(2,-7.2)
+y = complexmath.Complex(-9, 3.6)
+z = complexmath.Complex(15.4,-8)
+
+print(complexmath.Complex())
+print(x+y+z)
+print(sum([x,y,z],start=complexmath.Complex(0)))
