d2a7f691a332c42b5149b881763dbf63f83b909c
sandhusahib <sahibsandhu108@gmail.com>
2026-02-21 05:13:03 +0000

    Remove tests

--- complexmath.py
+++ complexmath.py
@@ -3,6 +3,7 @@ class Complex:
     def __init__(self, real_part=0, imag_part=0):
         self.r = real_part
         self.i = imag_part
+
     def __str__(self):
         if self.r == 0 and self.i == 0:
             complex_str = str(0)
@@ -31,12 +32,4 @@ class Complex:
         real_pt = (self.r * other.r) - (self.i * other.i)
         imag_pt = (self.r * other.i) + (self.i * other.r)
         return Complex(real_pt, imag_pt)
-
-
-if __name__ == "__main__":
-    x = Complex(2,-7.2)
-    y = Complex(-9, 3.6)
-    z = Complex(15.4,-8)
     
-    print(x + y + z)
-    print(sum([x,y,z]))
